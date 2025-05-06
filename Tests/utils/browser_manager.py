fcd3ad99f903c1b59a8acc98419e7c4ee09ee2a8
Mikita Bulankou <bulankovby@gmail.com>
2025-05-06 22:24:16 +0300

    fix: change option

--- Tests/utils/browser_manager.py
+++ Tests/utils/browser_manager.py
@@ -3,6 +3,6 @@ from selenium import webdriver
 
 def get_driver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
+    options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
     return driver
